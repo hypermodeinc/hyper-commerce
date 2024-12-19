6e48ca486beaaa5c7b8c18e3685f3da4d8ccebdd
Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>
2024-12-19 15:36:02 -0600

    Fix build error (#47)
    * update page params to use await
    
    * remove no store from action and await search params

--- frontend/app/product/[id]/page.tsx
+++ frontend/app/product/[id]/page.tsx
@@ -2,11 +2,17 @@ import { Suspense } from "react";
 import { ProductDetails } from "../../components/product-details";
 import { ProductSkeleton } from "../../components/skeletons";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+
   return (
     <div className="px-4">
       <Suspense fallback={<ProductSkeleton />}>
-        <ProductDetails id={params.id} />
+        <ProductDetails id={id} />
       </Suspense>
     </div>
   );
